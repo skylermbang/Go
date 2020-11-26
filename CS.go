801387eaa30b21dab0ad56ea11960f228bf64be3
Skyler Minsu Bang <32551541+skylermbang@users.noreply.github.com>
2020-11-26 22:34:53 +0900

    GO2

--- CS.go
+++ CS.go
@@ -8,5 +8,11 @@ import (
 func main() {
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 }
-
+// 2/17
 //  %g  to bring the arg 
+
+//3/17
+// exported name  start with capitalized letter 
+
+//4/17
+//func add(x int, y int) int {}  the types comes after the variable name 
--- math.go
+++ math.go
@@ -5,6 +5,13 @@ import (
 	"math"
 )
 
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+
+	fmt.Println(math.pi) // wont work pi x 
+	fmt.Println(math.Pi) // Pi  have to be capitalized letter 
 }
